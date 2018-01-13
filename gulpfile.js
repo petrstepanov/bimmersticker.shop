41926fababff6db9d7a8b509bd9edd10fdbe9c7b
petrstepanov <stepanovps@gmail.com>
2018-01-13 00:07:39 -0500

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber');
+    // replace = require('gulp-replace');
+//    imageResize = require('gulp-image-resize');
 
+// Scripts Task
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('scripts', function() {
+  gulp.src(['./node_modules/jquery/dist/jquery.js',
+            './node_modules/bootstrap/dist/js/bootstrap.js',
+            './js/main.js'
+    ])
+    .pipe(plumber()) // prevents breaking and has to go first here
+    .pipe(concat('bimmersticker.js'))
+    .pipe(uglify())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js/'));
 });
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/bimmersticker.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./js/*.js', ['scripts']);
+    gulp.watch('./sass/*.sass', ['styles']);
+});
+
+gulp.task('default', ['watch']);
