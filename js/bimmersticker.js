95450027edfdd1673c24e3a22aad12412739bd0e
petrstepanov <stepanovps@gmail.com>
2018-01-16 18:41:57 -0500

    Organizing client-side scripts

--- js/bimmersticker.js
+++ js/bimmersticker.js
@@ -14145,50 +14145,108 @@ Object.defineProperty(exports, '__esModule', { value: true });
 })));
 //# sourceMappingURL=bootstrap.js.map
 
-$(document).ready(function(){
-  // Wrap each character in heading in span
-  // $('.jumbotron-bimmersticker h1').html(function (i, html) {
-  //   var chars = $.trim(html).split("");
-  //   return '<span class="white-char">' + chars.join('</span><span class="white-char">') + '</span>';
-  // });
-  // $('.jumbotron-bimmersticker h1').html(function (i, html) {
-  //   return html.replace(/(\d)/g, '<span class="white-char">$1</span>');
-  // });
+var cardsManipulator = (function () {
+  var instance;
+  function init() {
+    // Private methods and variables
+    // function privateMethod(){
+    //     console.log( "I am private" );
+    // }
+    var comingSoonLinkText = "Soon on eBay";
+    var comingSoonLinkIcon = "<span class='icon-external'></span>";
+    var comingSoonBadgeHtml = "<img class='card-img-soon' src='img/coming-soon.svg' alt='coming soon'>";
 
-  // Select all links with hashes
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function(event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-            // Callback after animation
-            // Must change focus!
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { // Checking if the target was focused
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-              $target.focus(); // Set focus again
-            };
-          });
-        }
+    return {
+      // Public methods and variables
+      initComingSoonCards: function () {
+        $('.card-bimmersticker.coming-soon').each(function(){
+          // Create and attach the 'Coming Soon' badge
+          $(this).find('.card-img-top').after(comingSoonBadgeHtml);
+          // Make button disabled
+          $(this).find('.card-body a.btn').addClass('disabled');
+          // Change Button text to 'Soon...'
+          $(this).find('.card-body a.btn').html(comingSoonLinkIcon + ' ' + comingSoonLinkText);
+        });
       }
-    });
+    };
+  };
+  return {
+    getInstance: function () {
+      if ( !instance ) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+})();
+
+var htmlHelper = (function () {
+  var instance;
+  function init() {
+    // Private methods and variables
+    // function privateMethod(){
+    //     console.log( "I am private" );
+    // }
+    var slow = 600;
+    var normal = 400;
+    var fast = 200;
+    return {
+      // Public methods and variables
+      initSmoothLinkScrolling: function () {
+        $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+          // On-page links
+          if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+          ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+              // Only prevent default if animation is actually gonna happen
+              event.preventDefault();
+              $('html, body').animate({
+                scrollTop: target.offset().top
+              }, slow, function() {
+                // Callback after animation
+                // Must change focus!
+                // var $target = $(target);
+                // $target.focus();
+                // if ($target.is(":focus")) { // Checking if the target was focused
+                //   return false;
+                // } else {
+                //   $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                //   $target.focus(); // Set focus again
+                // };
+              });
+            }
+          }
+        });
+      }
+    };
+  };
+  return {
+    getInstance: function () {
+      if ( !instance ) {
+        instance = init();
+      }
+      return instance;
+    }
+  };
+})();
+
+$(document).ready(function(){
+  // Smooth link scrolling
+  var myHtmlHelper = htmlHelper.getInstance();
+  myHtmlHelper.initSmoothLinkScrolling();
+
+  // Coming soon cards logic
+  var myCardsManipulator = cardsManipulator.getInstance();
+  myCardsManipulator.initComingSoonCards();
 });
